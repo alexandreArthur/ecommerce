8c02f083bfeeb3a3d50a47a437ff09d9976ec7c1
Arthur alexandre <arthur_dda@hotmail.com>
2021-06-15 20:55:00 -0300

    BD de tests e criação automática da BD

--- src/main/java/com/ecommerce/domain/Categoria.java
+++ src/main/java/com/ecommerce/domain/Categoria.java
@@ -5,8 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
@@ -14,6 +19,8 @@ import java.io.Serializable;
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
