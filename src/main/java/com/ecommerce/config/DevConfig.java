6d533b1f476a400a0759c4714205f3a73eea0ce0
Arthur alexandre <arthur_dda@hotmail.com>
2021-07-09 19:36:20 -0300

    DBservice, DevConfig, TestConfig, application-dev, application-test e ajustes

--- src/main/java/com/ecommerce/config/DevConfig.java
+++ src/main/java/com/ecommerce/config/DevConfig.java
@@ -1,14 +1,25 @@
 package com.ecommerce.config;
 
+import com.ecommerce.services.DBservice;
 import com.ecommerce.services.EmailService;
 import com.ecommerce.services.SmtpEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.text.ParseException;
+
 @Configuration
 @Profile("dev")
 public class DevConfig {
+    @Autowired
+    private DBservice dBservice;
+    @Bean
+    public boolean instatianteDataBase() throws ParseException {
+        dBservice.instantiateTestDataBase();
+        return true;
+    }
     @Bean
     public EmailService emailService(){
         return new SmtpEmailService();
--- src/main/java/com/ecommerce/config/TestConfig.java
+++ src/main/java/com/ecommerce/config/TestConfig.java
@@ -1,14 +1,25 @@
 package com.ecommerce.config;
 
+import com.ecommerce.services.DBservice;
 import com.ecommerce.services.EmailService;
 import com.ecommerce.services.MockEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.text.ParseException;
+
 @Configuration
 @Profile("test")
 public class TestConfig {
+    @Autowired
+    private DBservice dBservice;
+    @Bean
+    public boolean instatianteDataBase() throws ParseException {
+        dBservice.instantiateTestDataBase();
+        return true;
+    }
 
     @Bean
     public EmailService emailService(){
