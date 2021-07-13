474f8c77e5fd67c0d788937dd3f16aa18c65eef8
Arthur alexandre <arthur_dda@hotmail.com>
2021-07-13 16:55:22 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.ecommerce.config;
 
+import com.ecommerce.security.JWTAuthenticationFilter;
+import com.ecommerce.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -21,6 +25,10 @@ import java.util.Arrays;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private Environment env;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JWTUtil jwTutil;
 
     private static final String[] PUBLIC_MATCHERS ={
 
@@ -45,7 +53,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwTutil));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
---
+++ src/main/java/com/ecommerce/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ecommerce.services;
+
+import com.ecommerce.domain.Cliente;
+import com.ecommerce.repositories.ClienteRepository;
+import com.ecommerce.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Cliente cli = clienteRepository.findByEmail(email);
+        if (cli==null){
+            throw new UsernameNotFoundException(email);
+        }
+        return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfil());
+    }
+}
