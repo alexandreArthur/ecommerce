ae02cce11ebbc39cebf247069473f187bbfa0a62
Arthur alexandre <arthur_dda@hotmail.com>
2021-07-23 16:42:26 -0300

    Expondo header location nas respostas

---
+++ src/main/java/com/ecommerce/filters/HeaderExposureFilter.java
@@ -0,0 +1,22 @@
+package com.ecommerce.filters;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HeaderExposureFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        res.addHeader("access-control-expose-headers", "location");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
