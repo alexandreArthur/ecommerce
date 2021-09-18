c3f940e34a5c35011df67c98fc001a50b088df27
Arthur Alexandre <arthur_dda@hotmail.com>
2021-09-18 10:32:10 -0300

    falta do component impedia o front conseguir acesso ao header com o location do pedido confirmado

--- src/main/java/com/ecommerce/filters/HeaderExposureFilter.java
+++ src/main/java/com/ecommerce/filters/HeaderExposureFilter.java
@@ -1,9 +1,12 @@
 package com.ecommerce.filters;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class HeaderExposureFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
